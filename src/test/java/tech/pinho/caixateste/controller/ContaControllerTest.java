b088ceafd9a611d9d2e53cf860b4deb10a1ec1c0
Bruno <bruno.pereira.pinho@gmail.com>
2026-05-04 21:35:11 -0300

    Conta controller

--- src/test/java/tech/pinho/caixateste/controller/ContaControllerTest.java
+++ src/test/java/tech/pinho/caixateste/controller/ContaControllerTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import tech.pinho.caixateste.domain.Conta;
 import tech.pinho.caixateste.service.ContaService;
 
@@ -14,6 +18,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class ContaControllerTest {
@@ -24,6 +31,13 @@ class ContaControllerTest {
     @InjectMocks
     private ContaController contaController;
 
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(contaController).build();
+    }
+
     @Test
     void test() {
         when(contaService.listar()).thenReturn(Collections.emptyList());
@@ -33,6 +47,21 @@ class ContaControllerTest {
         assertTrue(contas.isEmpty());
         verify(contaService, times(1)).listar();
         verify(contaService, times(0)).salvar(any());
+    }
 
+    @Test
+    void test_mock_mvc() throws Exception {
+        when(contaService.listar()).thenReturn(Collections.emptyList());
+
+        String contas = mvc.perform(
+                        get("/contas")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        assertEquals("[]", contas);
+        verify(contaService, times(1)).listar();
+        verify(contaService, times(0)).salvar(any());
     }
 }
